7bd6ea5b615cc32dd31fb9721fe7f4081477ac9a
Yunus Can Erol <ycanerol@gmail.com>
2017-11-08 14:29:34 +0100

    Add mexican hat RF plot

--- vary_SD.py
+++ vary_SD.py
@@ -8,17 +8,57 @@ Created on Tue Nov  7 10:36:16 2017
 import numpy as np
 import matplotlib.pyplot as plt
 
+<<<<<<< Updated upstream
 slims = np.arange(0, 10, step=1)
+=======
+plt.style.use('ggplot')
+>>>>>>> Stashed changes
 
-for i, item in enumerate(slims, 1):
+slims = np.arange(0, 10, step=.4)
+filters = np.zeros((len(slims),20))
+
+for i in range(1, len(slims)):
     # Define upper and lower bounds
+<<<<<<< Updated upstream
     upper = item
     lower = slims[i-2]
     mask = np.logical_not(np.logical_and(-Zm >= lower, -Zm <= upper))
+=======
+    upper = slims[i]
+    lower = slims[i-1]
+    mask = np.logical_not(np.logical_and(Zm >= lower, Zm <= upper))
+>>>>>>> Stashed changes
     mask3d = np.broadcast_arrays(sta, mask[..., None])[1]
-    plt.plot(np.mean(np.ma.array(sta, mask=mask3d), axis=(0, 1)) - i/20,
+    component = np.mean(np.ma.array(sta, mask=mask3d), axis=(0, 1))
+    filters[i-1] = component
+    offset = -i/20
+    plt.plot(component + offset,
              label=str(upper))
+<<<<<<< Updated upstream
     plt.text(-3, -i/20, '{:2.2f}-{:2.2f}'.format(lower, upper))
+=======
+    plt.axhline(offset, alpha=.4, color='k', linestyle='dashed')
+    plt.text(-3, offset, '{:2.2f}-{:2.2f}'.format(lower, upper), size=8)
+>>>>>>> Stashed changes
 #plt.legend()
 plt.axis('off')
+plt.show()
+
+# %%
+
+peaks = np.argmax(np.abs(filters), axis=1)
+mexican = np.array([])
+for peak, i in enumerate(peaks):
+    mexican = np.append(mexican, filters[peak, i])
+mexican = np.append(mexican[::-1], mexican[1:])
+slims2 = np.append(-slims[::-1], slims[1:])
+ax = plt.subplot(111)
+ax.plot(slims2, mexican)
+plt.xlabel('Distance from RF center[$\sigma$]')
+plt.ylabel('Relative sensitivity')
+plt.title('Mexican hat?')
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+ax.spines['left'].set_visible(False)
+ax.spines['bottom'].set_visible(False)
 plt.show()
