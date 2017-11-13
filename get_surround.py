58238ef8d736bf361cf48b28a47f2315618bd869
Yunus Can Erol <ycanerol@gmail.com>
2017-11-13 15:05:42 +0100

    Collect functions in a separate .py file called miscfuncs.py

--- get_surround.py
+++ get_surround.py
@@ -8,85 +8,17 @@ Created on Tue Oct 10 11:51:38 2017
 
 import numpy as np
 import matplotlib.pyplot as plt
-import glob
 import gaussfitter as gfit
 
+try:
+    import miscfuncs as mf
+except ImportError:
+    import sys
+    sys.path.append('/home/ycan/Documents/scripts/')
+    import miscfuncs as mf
 
-def show_sta(sta, max_i, f_size=10):
-    plt.plot(figsize=(12, 12), dpi=200)
-    sta_min = np.min(sta)
-    sta_max = np.max(sta)
-
-    for i in range(20):
-        plt.subplot(4, 5, i+1)
-
-        plt.imshow(sta[:, :, i], vmin=sta_min, vmax=sta_max , cmap='Greys')
-        plt.axis('off')
-    plt.show()
-
-
-def readexps(directory, test=False):
-    # TODO: stimulus_order needs to be parametrized/automated properly
-    stimulus_order = 5
-    file_paths = glob.glob(directory+'*/analyzed/{}*.npz'.format(stimulus_order))
-    file_paths = sorted(file_paths)
-    exp_names = [i.split('/')[-3] for i in file_paths]
-    clusters = [i.split('/')[-1].split('C')[-1].split('.')[0] for i in file_paths]
-
-    files = np.array([file_paths, exp_names, clusters])
-
-    # Use only one file for testing
-    if test:
-        files = files[:, np.random.randint(files.shape[1])]
-        files = files[:, np.newaxis]
-
-    return files
-
-
-def ringmask(data, center_px, r):
-    # Create a ring shaped mask  in spatial dimention
-    # and apply to the STA along time axis
-
-    # HINT: "True" masks the value out
-    mask = np.array([True]*data.size).reshape(data.shape)
-
-    cx, cy, _ = center_px
-
-    # Check if the specified ring size is larger than the shape
-    outofbounds = (cx+r > data.shape[0]-1 or cx-r < 0 or
-                   cx+r > data.shape[0]-1 or cx-r < 0)
-
-    mask[cx-r:cx+r+1, cy-r:cy+r+1, :] = False
-    mask[cx-(r-1):cx+(r), cy-(r-1):cy+(r), :] = True
-
-    masked_data = np.ma.array(data, mask=mask)
-
-    if outofbounds: masked_data = None
-
-    return masked_data, outofbounds
-
-
-def cut_around_center(sta_original, max_i_o, f_size):
-    try:
-        from lnp_checkerflicker import check_max_i
-    except ImportError:
-        import sys
-        sys.path.append('/home/ycan/Documents/scripts/modules')
-        from lnp_checkerflicker import check_max_i
-    if f_size is not 0:
-        max_i_o = check_max_i(sta_original, max_i_o, f_size=f_size)
-        sta = sta_original[max_i_o[0]-f_size:max_i_o[0]+f_size+1,
-                           max_i_o[1]-f_size:max_i_o[1]+f_size+1,
-                           :]
-        max_i = np.append([f_size]*2, max_i_o[2])
-
-    else:
-        sta = sta_original
-        max_i = max_i_o
-    return sta, max_i
-
-
-files = readexps(directory='/home/ycan/Documents/data/2017-08-02', test=True)
+files = mf.readexps(directory='/home/ycan/Documents/data/2017-08-02',
+                    test=True)
 #files = np.reshape(files[:, 25], (3,1))
 
 for i in range(files.shape[1]):
@@ -104,13 +36,12 @@ for i in range(files.shape[1]):
 
     f_size = 10
 
-    sta, max_i = cut_around_center(sta_original, max_i_original, f_size)
+    sta, max_i = mf.cut_around_center(sta_original, max_i_original, f_size)
 
     # Isolate the frame that wtll be used to fit the 2D Gaussian distribution
     # This is a first pass approach, just to see if anything comes out
     fit_frame = sta[:, :, max_i[2]]
 #    show_sta(sta, max_i)
-
 
     # %% Concenctric rings using pixel distances
 
@@ -122,14 +53,14 @@ for i in range(files.shape[1]):
     for i in range(ring_sizes1[0], ring_sizes1[1]):
         plt.subplot(4, 2, 1)
         plt.title('Temporal filter at n pixel distance from center pixel')
-        masked = ringmask(sta, max_i, i)
+        masked = mf.ringmask(sta, max_i, i)
         if not masked[1]:
             plt.plot(np.mean(masked[0], axis=(0, 1)), label=str(i))
         plt.legend()
 
     for i in range(ring_sizes2[0], ring_sizes2[1]):
         plt.subplot(4, 2, 3)
-        masked = ringmask(sta, max_i, i)
+        masked = mf.ringmask(sta, max_i, i)
         if not masked[1]:
             plt.plot(np.mean(masked[0], axis=(0, 1)), label=str(i))
     plt.legend()
---
+++ modules/miscfuncs.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 13 14:58:16 2017
+
+@author: ycan
+
+Collection of functions that are used for various stages of analysis
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+
+
+def show_sta(sta, max_i, f_size=10):
+    plt.plot(figsize=(12, 12), dpi=200)
+    sta_min = np.min(sta)
+    sta_max = np.max(sta)
+
+    for i in range(20):
+        plt.subplot(4, 5, i+1)
+
+        plt.imshow(sta[:, :, i], vmin=sta_min, vmax=sta_max, cmap='Greys')
+        plt.axis('off')
+    plt.show()
+
+
+def readexps(directory, test=False):
+    # TODO: stimulus_order needs to be parametrized/automated properly
+    stimulus_order = 5
+    file_paths = glob.glob(directory +
+                           '*/analyzed/{}*.npz'.format(stimulus_order))
+    file_paths = sorted(file_paths)
+    exp_names = [i.split('/')[-3] for i in file_paths]
+    clusters = [i.split('/')[-1].split('C')[-1].split('.')[0] for i in file_paths]
+
+    files = np.array([file_paths, exp_names, clusters])
+
+    # Use only one file for testing
+    if test:
+        files = files[:, np.random.randint(files.shape[1])]
+        files = files[:, np.newaxis]
+
+    return files
+
+
+def ringmask(data, center_px, r):
+    # Create a ring shaped mask  in spatial dimention
+    # and apply to the STA along time axis
+
+    # HINT: "True" masks the value out
+    mask = np.array([True]*data.size).reshape(data.shape)
+
+    cx, cy, _ = center_px
+
+    # Check if the specified ring size is larger than the shape
+    outofbounds = (cx+r > data.shape[0]-1 or cx-r < 0 or
+                   cx+r > data.shape[0]-1 or cx-r < 0)
+
+    mask[cx-r:cx+r+1, cy-r:cy+r+1, :] = False
+    mask[cx-(r-1):cx+(r), cy-(r-1):cy+(r), :] = True
+
+    masked_data = np.ma.array(data, mask=mask)
+
+    if outofbounds:
+        masked_data = None
+
+    return masked_data, outofbounds
+
+
+def cut_around_center(sta_original, max_i_o, f_size):
+    try:
+        from lnp_checkerflicker import check_max_i
+    except ImportError:
+        import sys
+        sys.path.append('/home/ycan/Documents/scripts/modules')
+        from lnp_checkerflicker import check_max_i
+    if f_size is not 0:
+        max_i_o = check_max_i(sta_original, max_i_o, f_size=f_size)
+        sta = sta_original[max_i_o[0]-f_size:max_i_o[0]+f_size+1,
+                           max_i_o[1]-f_size:max_i_o[1]+f_size+1,
+                           :]
+        max_i = np.append([f_size]*2, max_i_o[2])
+
+    else:
+        sta = sta_original
+        max_i = max_i_o
+    return sta, max_i
