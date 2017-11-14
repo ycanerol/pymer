fa4b78a3c37c234dccb44b5ac20568ad17038416
Yunus Can Erol <ycanerol@gmail.com>
2017-11-14 20:17:46 +0100

    Change vary_SD for biweekly meeting report

--- vary_SD.py
+++ vary_SD.py
@@ -8,10 +8,13 @@ Created on Tue Nov  7 10:36:16 2017
 import numpy as np
 import matplotlib.pyplot as plt
 
-plt.style.use('ggplot')
+plt.style.use('default')
 
-slims = np.arange(0, 10, step=.4)
+slims = np.arange(0, 6, step=.3)
 filters = np.zeros((len(slims),20))
+
+fig = plt.figure(dpi=120)
+plt.suptitle('{}-{:0>5}'.format(exp_date, cluster))
 
 for i in range(1, len(slims)):
     # Define upper and lower bounds
@@ -22,14 +25,13 @@ for i in range(1, len(slims)):
     component = np.mean(np.ma.array(sta, mask=mask3d), axis=(0, 1))
     filters[i-1] = component
     offset = -i/20
+    plt.subplot(1, 2, 1)
     plt.plot(component + offset,
              label=str(upper))
     plt.axhline(offset, alpha=.4, color='k', linestyle='dashed')
-    plt.text(-3, offset, '{:2.2f}-{:2.2f}'.format(lower, upper), size=8)
+    plt.text(-5, offset, '{:2.2f}-{:2.2f}'.format(lower, upper), size=7)
 #plt.legend()
 plt.axis('off')
-plt.show()
-
 # %%
 
 peaks = np.argmax(np.abs(filters), axis=1)
@@ -38,13 +40,17 @@ for peak, i in enumerate(peaks):
     mexican = np.append(mexican, filters[peak, i])
 mexican = np.append(mexican[::-1], mexican[1:])
 slims2 = np.append(-slims[::-1], slims[1:])
-ax = plt.subplot(111)
+ax = plt.subplot(1, 2, 2)
 ax.plot(slims2, mexican)
 plt.xlabel('Distance from RF center[$\sigma$]')
-plt.ylabel('Relative sensitivity')
+plt.ylabel('Peak of temporal filter')
 plt.title('Mexican hat?')
 ax.spines['right'].set_visible(False)
 ax.spines['top'].set_visible(False)
 ax.spines['left'].set_visible(False)
 ax.spines['bottom'].set_visible(False)
-plt.show()
+
+plt.tight_layout()
+plt.savefig('/home/ycan/Documents/notes/2017-11-15/'
+            'varysd/{}-{:0>5}.svg'.format(exp_date, cluster),
+            format='svg', dpi='figure', bbox_inches='tight')
