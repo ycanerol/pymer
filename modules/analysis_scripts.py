dd873425d09b1180bd61c9262048d6c65a9d1520
Yunus Can Erol <ycanerol@gmail.com>
2017-11-28 19:15:12 +0100

    Add new function for frametimes

--- modules/analysis_scripts.py
+++ modules/analysis_scripts.py
@@ -77,3 +77,125 @@ def read_ods(ods_fpath, cutoff=4):
         metadata_dict[metadata[0][i]] = metadata[1][i]
 
     return clusters, metadata_dict
+
+
+def getframetimes(filepath, threshold=75, sampling_rate=10000, plotting=False,
+                  time_offset=25, zeroADvalue=32768,
+                  microvoltsperADunit=25625/2048):
+    """
+    Extract frame timings from the triggered signal recorded alongside the
+    MEA data.
+
+    Typically the timing data is in the file <stimulusnr>_253.bin. It comes in
+    pulses; onset and offset of the pulse denotes different things depending
+    on the stimulus code.
+
+    The most typical case is that pulse onset corresponds to when a frame comes
+    on screen, and the pulse is turned off at the next frame (even if the
+    consecutive frame is identical). In this case the duration of the pulse
+    will be 1000 ms/refresh rate; which is ~16.6 ms for 60 Hz refresh rate.
+    For these type of stimuli, using only pulse onsets is sufficient.
+
+    For some types of stimuli, the pulse offset is also important, for
+    these cases pulse onsets and offsets need to be used together.
+
+    There is also a delay between the pulse and the frame actually being
+    displayed, which should be accounted for. The delay is 25 ms for setup
+    Bilbo.
+
+    Parameters:
+    ----------
+        filepath:
+            Path to the binary file. '.../<stimulus_nr>_253.bin'
+        threshold:
+            The threshold in milivolts for the trigger signal. Default is
+            75 mV.
+        sampling_rate:
+            Sampling rate of the recording in Hz.
+        plotting:
+            Whether to plot the whole trace and signal on-offsets. Slow for
+            long recordings and frequent pulses (e.g. checkerflicker). Default
+            is False.
+        time_offset:
+            The delay between the pulse generation and the disply actually
+            updating. Default is 25 ms.
+        zeroADvalue:
+            The zero point of the analog digital conversion. Copied directly
+            from frametimings10.m by Norma(?). Default is 32768.
+        microvoltsperADunit:
+            Conversion factor between the readout and voltage. Resulting
+            conversion result is in milivolts. Default is 25625/2048.
+
+
+    Returns:
+    -------
+        frametimings_on:
+            List of times in miliseconds where a pulse started, corresponding
+            to a frame update. Corrected for the monitor delay by time_offset.
+        frametimings_off:
+            List of times in miliseconds where a pulse ended. Not to be used
+            frequently, only if a particular stimulus requires it.
+
+
+    """
+    import struct
+
+    with open(filepath, mode='rb') as file:  # b is important -> binary
+        fileContent = file.read()
+    # %%
+
+    # The header contains the length of the recording as a unsigned 32 bit int
+    length = struct.unpack('I', fileContent[:4])[0]
+    # The rest of the binary data is the voltage trace, as unsigned 16 bit int
+    voltage_raw = np.array(struct.unpack('H'*length, fileContent[16:]))
+
+    # Convert units to microvolts, using constants from Norma(?)'s script
+    voltage = (voltage_raw - zeroADvalue) / microvoltsperADunit
+    # Set the baseline value to zero
+    voltage = voltage - voltage[voltage < threshold].mean()
+    # %%
+    time = np.arange(length) / (sampling_rate * 1e-3)  # In miliseconds
+    time = time + time_offset  # Correct for the time delay
+
+    def thr_cros(array, threshold):
+        size = array.shape[0]
+        oncross = np.array([False]*size)
+        offcross = np.array([False]*size)
+        for i in range(size-1):
+            if array[i-1] < threshold and array[i] > threshold:
+                oncross[i] = True
+            if array[i-1] > threshold and array[i] < threshold:
+                offcross[i] = True
+        return oncross, offcross
+
+    print('Total recording time: {:6.1f} seconds'
+          ' (= {:3.1f} minutes)'.format(length/sampling_rate,
+                                        (length/sampling_rate)/60))
+
+    onsets, offsets = thr_cros(voltage, threshold)
+    if onsets.sum() != offsets.sum():
+        print('Number of pulse onset and offsets are not equal!')
+    if plotting:
+        import matplotlib.pyplot as plt
+        # Plot the whole voltage trace
+        plt.figure(figsize=(10, 10))
+        plt.plot(time, voltage)
+        plt.show()
+
+        # Put all stimulus onset and offsets on top of each other
+        # This part takes very long time for long recordings
+        plt.figure()
+        for i in range(onsets.shape[0]):
+            if onsets[i]:
+                plt.subplot(211)
+                plt.plot(voltage[i-2:i+3])
+            if offsets[i]:
+                plt.subplot(212)
+                plt.plot(voltage[i-2:i+3])
+        plt.show()
+        plt.close()
+
+    frametimings_on = time[onsets]
+    frametimings_off = time[offsets]
+
+    return frametimings_on, frametimings_off
