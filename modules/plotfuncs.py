189706c773287de2636004a0a01ee9ebc97bf0b1
Yunus Can Erol <ycanerol@gmail.com>
2017-12-22 12:55:30 +0100

    Add function to get ideal arrangement of subplots given a total number of plots needed.

--- modules/plotfuncs.py
+++ modules/plotfuncs.py
@@ -65,3 +65,60 @@ def RFcolormap(colors=None):
                   "#2A309E","#232AA0","#1A24A2")
     cm = matplotlib.colors.ListedColormap(colors)
     return cm
+
+def numsubplots(n, recursive=False):
+    """
+    Define the best arrangement of subplots for a
+    given number of plots.
+
+    Parameters:
+        n:
+            Number of total plots needed.
+        recursive:
+            Whether to return current number of subplots.
+            Used for recursive calls.
+    Returns:
+        p:
+            A list containing the ideal arrangement
+            of subplots.
+        n:
+            Current number of subplots. Returned only
+            when recursively calling the function.
+
+    Ported to Python by Yunus Can Erol on Dec 2017
+    from mathworks.com/matlabcentral/fileexchange/
+    26310-numsubplots-neatly-arrange-subplots
+    Original by Rob Campbell, Jan 2010
+
+    """
+    import primefac
+
+    while primefac.isprime(n) and n > 4:
+        n += 1
+
+    p = primefac.prime_factorize(n)
+    if len(p) == 1:
+        p = [1] + p
+        if recursive:
+            return p, n
+        else:
+            return p
+
+    while len(p) > 2:
+        if len(p) >= 4:
+            p[0] = p[0]*p[-2]
+            p[1] = p[1]*p[-1]
+            del p[-2:]
+        else:
+            p[0] = p[0]*p[1]
+            del p[1]
+        p = sorted(p)
+
+    while p[0]/p[1] > 2.5:
+        N = n+1
+        p, n = numsubplots(N, recursive=True)
+
+    if recursive:
+        return p, n
+    else:
+        return p
