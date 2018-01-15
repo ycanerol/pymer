dac4cfbf4dea73e1144ba4813c3a68f54137f987
Yunus Can Erol <ycanerol@gmail.com>
2018-01-15 17:31:40 +0100

    Add error for cut_around_center to account for noisy cells

--- modules/miscfuncs.py
+++ modules/miscfuncs.py
@@ -101,6 +101,13 @@ def ringmask(data, center_px, r):
 def cut_around_center(sta_original, max_i_o, f_size):
     if f_size+2 > (sta_original.shape[0])/2 or f_size+2 > (sta_original.shape[1])/2:
         raise ValueError('Frame size is larger than STA dimensions')
+    if (max_i_o[0] + f_size > sta_original.shape[0] or
+        max_i_o[0] - f_size < 1):
+        raise ValueError('Frame is out of the STA dimension')
+    if (max_i_o[1] + f_size > sta_original.shape[1] or
+        max_i_o[1] - f_size < 1):
+        raise ValueError('Frame is out of the STA dimension')
+
     if f_size is not 0:
         sta = sta_original[max_i_o[0]-f_size:max_i_o[0]+f_size+1,
                            max_i_o[1]-f_size:max_i_o[1]+f_size+1,
