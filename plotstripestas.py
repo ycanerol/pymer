c3e210b35e3402660f3b939af87424fe77237134
Yunus Can Erol <ycanerol@gmail.com>
2018-02-05 18:08:15 +0100

    Plot all of the STA for stripeflicker

--- plotstripestas.py
+++ plotstripestas.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 import plotfuncs as plf
 import numpy as np
 import analysis_scripts as asc
-import miscfuncs as msc
 import os
 
 
@@ -31,7 +30,6 @@ def plotstripestas(exp_name, stim_nrs):
 
         clusters = data['clusters']
         stas = data['stas']
-        max_inds = data['max_inds']
         filter_length = data['filter_length']
         stx_w = data['stx_w']
         exp_name = data['exp_name']
@@ -43,24 +41,19 @@ def plotstripestas(exp_name, stim_nrs):
 
         # Determine frame size so that the total frame covers
         # an area large enough i.e. 2*700um
-        fsize = int(700/(stx_w*px_size))
         t = np.arange(filter_length)*frame_duration*1000
-        vscale = fsize * stx_w*px_size
+        vscale = int(stas[0].shape[0] * stx_w*px_size/1000)
         for i in range(clusters.shape[0]):
             sta = stas[i]
-            max_i = max_inds[i]
-            try:
-                sta, max_i = msc.cutstripe(sta, max_i, fsize)
-            except ValueError:
-                continue
+
             vmax = np.max(np.abs(sta))
             vmin = -vmax
-            plt.figure(figsize=(6, 6))
+            plt.figure(figsize=(6, 15))
             ax = plt.subplot(111)
             im = ax.imshow(sta, cmap='RdBu', vmin=vmin, vmax=vmax,
                            extent=[0, t[-1], -vscale, vscale], aspect='auto')
             plt.xlabel('Time [ms]')
-            plt.ylabel('Distance [µm]')
+            plt.ylabel('Distance [mm]')
 
             plf.spineless(ax)
             plf.colorbar(im, ticks=[vmin, 0, vmax], format='%.2f', size='2%')
@@ -71,12 +64,12 @@ def plotstripestas(exp_name, stim_nrs):
                                                        clusterids[i],
                                                        clusters[i][2],
                                                        quals[i]))
-            plt.subplots_adjust(top=.85)
+            plt.subplots_adjust(top=.90)
             savepath = os.path.join(exp_dir, 'data_analysis',
                                     stimname, 'STAs')
             if not os.path.isdir(savepath):
                 os.makedirs(savepath, exist_ok=True)
-            plt.savefig(os.path.join(savepath, clusterids[i]+'.svg'))
+            plt.savefig(os.path.join(savepath, clusterids[i]+'.svg'),
+                        bbox_inches='tight')
             plt.close()
         print(f'Plotting of {stimname} completed.')
-
