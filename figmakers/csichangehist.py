8b34f865f21c14ae96b3c912c974ae39afc39870
Yunus Can Erol <ycanerol@gmail.com>
2018-03-10 11:20:33 +0100

    Add preliminary non-paired non-parametric test for comparing populations

--- figmakers/csichangehist.py
+++ figmakers/csichangehist.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import texplot
 import plotfuncs as plf
+from scipy import stats
 
 data = np.load('/home/ycan/Documents/thesis/analysis_auxillary_files/'
                'thesis_csiplotting.npz')
@@ -27,13 +28,22 @@ for i, color in enumerate(colorcategories):
     group = [index for index, c in enumerate(colors) if c == color]
     ax = axes[i]
     change = csichange[group]
+    if i == 0:
+        csichange_on = np.copy(change)
+    elif i == 1:
+        csichange_off = np.copy(change)
     ax.hist(change, color=color , bins=bins)
     ax.set_ylim([0, 10])
     ax.set_yticks(np.linspace(0, 8, 3))
     ax.plot([0, 0], [0, 12], 'r--', alpha=.3)
     plf.subplottext(['A', 'B', 'C', 'D', 'E'][i], ax, x=-.08)
     plf.spineless(ax)
+    print(i, stats.wilcoxon(group)[1])
 
 plt.subplots_adjust(hspace=.3)
 texplot.savefig('csihistogram_mp')
 plt.show()
+
+manwhitu = stats.mannwhitneyu(csichange_on, csichange_off)
+print('Mann-Whitney U test for ON and OFF populations '
+      f'being different: {manwhitu[1]:5.4f}')
