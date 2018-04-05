ea1235c330756b9cdd58da417d047a3b78f79609
Yunus Can Erol <ycanerol@gmail.com>
2018-04-06 00:09:18 +0200

    Try new CSI definition for scatterplot
    Background:
    I realized the three OFF cells that seem to decrease CSI
    with increasing light level were actually bad fits. To fix
    it I decided to try a new definition of the CSI
    
    New definition was based on the sum of fitted Gaussians
    CSI = abs(sum(negative part))/sum(positive part)
    
    Turned out, it fixes the bad fitted cells but introduced its
    own problems.
    
    Resolution:
    I decided to stick with the old definition.

--- figmakers/scratch_stripesurroundshow.py
+++ figmakers/scratch_stripesurroundshow.py
@@ -21,18 +21,25 @@ import numpy as np
 from stripesurround import onedgauss
 
 
-toshow = [
-        ('20180124', '04001', 'offweird1'),
-        ('20180124', '07401', 'offweird2'),
-        ('20180207', '02001', 'offweird3'),
-        ('20180207', '03002', 'offweird4'),
-        ]
+#toshow = [
+#        ('20180124', '04001', 'offweird1'),
+#        ('20180124', '07401', 'offweird2'),
+#        ('20180207', '02001', 'offweird3'),
+#        ('20180207', '03002', 'offweird4'),
+#        ]
+
+data = np.load('/home/ycan/Documents/thesis/analysis_auxillary_files/'
+               'thesis_csiplotting.npz')
+cells = data['cells']
+colors = data['colors']
+csi_alts = np.ones(data['csi'].shape)*-1
 
 rows = 2
 columns = 2
 
-for i, (exp_name, clustertoplot, label) in enumerate(toshow):
-
+#for i, (exp_name, clustertoplot, label) in enumerate(toshow):
+for i, (exp_name, clustertoplot) in enumerate(cells):
+#    break
     if '20180124' in exp_name or '20180207' in exp_name:
         stripeflicker = [6, 12]
     elif '20180118' in exp_name:
@@ -83,7 +90,6 @@ for i, (exp_name, clustertoplot, label) in enumerate(toshow):
         fsize_diff = fsize_original - fsize
         t = np.arange(filter_length)*frame_duration*1000
         vscale = fsize * stx_w*px_size
-interacting with incident photons,
 
         sta, max_i = msc.cutstripe(sta, max_i, fsize*2)
 
@@ -110,10 +116,34 @@ interacting with incident photons,
         ax2.set_xticks([])
         ax2.plot(onoroff*fitv, -s, label='Data')
         # Displace the center of both distributions according to the difference
+        fit = fit*onoroff
+        surround_alt = np.abs(fit[fit<0].sum())
+        center_alt = np.abs(fit[fit>0].sum())
+        csi_alt = np.abs(fit[fit<0].sum())/(fit[fit>0].sum())
+        csi_alts[j, i] = csi_alt
+        print(surround_alt, center_alt, csi_alt)
+        fit_cut = fit[2*fsize_diff:-2*fsize_diff]
+        plf.subplottext(f'csi_alt: {csi_alt:4.2f}',
+                ax2, x=.95, y=.05, fontsize=8, fontweight='normal')
+        plf.subplottext(['', '■'][j], ax2, x=.50, y=.05, color=colors[i],
+                        fontsize=8)
         popt = popt - np.array([0, 1, 0, 0, 1, 0])*fsize_diff*2
         ax2.plot(onedgauss(s, *popt[:3]), -s,  '--', label='Center')
         ax2.plot(-onedgauss(s, *popt[3:]), -s,  '--', label='Surround')
+        ax2.plot(fit_cut, -s)
 
     plt.subplots_adjust(hspace = .3, wspace=.2)
+    plt.suptitle(f'{i:>5d} {exp_name} {clustertoplot}')
 #    texplot.savefig(label)
     plt.show()
+#%%
+
+ax = plt.subplot(111)
+ax.scatter(csi_alts[0, :], csi_alts[1, :], c=colors, s=20, alpha=.8)
+ax.plot([0, csi_alts.max()], [0, csi_alts.max()])
+ax.set_aspect('equal')
+axis_lims = [-.03, 1.5]
+#axis_lims = None
+ax.set_xlim(axis_lims)
+ax.set_ylim(axis_lims)
+plt.show()
