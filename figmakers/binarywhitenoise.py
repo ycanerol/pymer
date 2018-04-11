31d810dd8b107aa2f3d96ba8ff94ad12164da26c
Yunus Can Erol <ycanerol@gmail.com>
2018-04-11 21:45:30 +0200

    Remove boundaries from example stimulus generator

--- figmakers/binarywhitenoise.py
+++ figmakers/binarywhitenoise.py
@@ -7,20 +7,27 @@ Created on Sun Mar 11 23:45:45 2018
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import plotfuncs as plf
 
-s = 40
-savedir = '/home/ycan/Downloads/'
-
-np.random.seed(0)
-plt.matshow(np.random.randint(0, 2, s*s).reshape(s,s), cmap='Greys');
-plt.xticks([]);
-plt.yticks([]);
-plt.savefig(savedir+'checker.pdf', bbox_inches='tight')
+s = 10
+frames = 5
+savedir = '/home/ycan/Downloads/whitenoisestim/'
 
 np.random.seed(0)
-a = np.repeat(np.random.randint(0, 2, s), s)
-a = a.reshape(s,s)
-plt.matshow(a, cmap='Greys')
-plt.xticks([])
-plt.yticks([])
-plt.savefig(savedir+'stripe.pdf', bbox_inches='tight')
+for i in range(frames):
+    plt.matshow(np.random.randint(0, 2, s*s).reshape(s,s), cmap='Greys');
+    plt.xticks([])
+    plt.yticks([])
+    plf.spineless(plt.gca())
+    plt.savefig(savedir+f'checker{i}.svg', bbox_inches='tight', pad_inches=0)
+
+np.random.seed(0)
+
+for i in range(frames):
+    a = np.repeat(np.random.randint(0, 2, s), s)
+    a = a.reshape(s,s)
+    plt.imshow(a, cmap='Greys')
+    plt.xticks([])
+    plt.yticks([])
+    plf.spineless(plt.gca())
+    plt.savefig(savedir+f'stripe{i}.svg', bbox_inches='tight', pad_inches=0)
