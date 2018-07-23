91e2d3b64662ab3db8ece321f4397cfce819b17e
Yunus Can Erol <ycanerol@gmail.com>
2018-07-23 19:39:43 +0200

    Improve frametimesfrommat to take monitor delay into account

--- frametimesfrommat.py
+++ frametimesfrommat.py
@@ -10,37 +10,42 @@ import os
 import numpy as np
 import scipy.io
 import iofuncs as iof
+import analysis_scripts as asc
 
-exp_dir = iof.exp_dir_fixer('V')
-"""
-Extract frame times from .mat files. Needed for analyzing data
-from other people, binary files containing the frame time pulses
-are not usually available.
+def frametimesfrommat(exp_name):
+    """
+    Extract frame times from .mat files. Needed for analyzing data
+    from other people, binary files containing the frame time pulses
+    are not usually available.
+    """
+    exp_dir = iof.exp_dir_fixer(exp_name)
 
-"""
-for i in range(1, 100):
-    try:
-        name = iof.getstimname(exp_dir, i)
-    except IndexError as e:
-        if str(e).startswith('Stimulus'):
-            break
-        else:
-            raise
+    _, metadata = asc.read_ods(exp_dir)
+    monitor_delay = metadata['monitor_delay(s)']
 
-    matfile = os.path.join(exp_dir, 'frametimes', name + '_frametimings.mat')
+    for i in range(1, 100):
+        try:
+            name = iof.getstimname(exp_dir, i)
+        except IndexError as e:
+            if str(e).startswith('Stimulus'):
+                break
+            else:
+                raise
 
-    try:
-        f = scipy.io.matlab.loadmat(matfile)
-        ftimes = f['ftimes'][0, :]
-    except NotImplementedError:
-        import h5py
-        with h5py.File(matfile, mode='r') as f:
-            ftimes = f['ftimes'][:]
-            if len(ftimes.shape) != 1:
-                ftimes = ftimes.flatten()
+        matfile = os.path.join(exp_dir, 'frametimes', name + '_frametimings.mat')
 
-    ftimes = ftimes/1000
+        try:
+            f = scipy.io.matlab.loadmat(matfile)
+            ftimes = f['ftimes'][0, :]
+        except NotImplementedError:
+            import h5py
+            with h5py.File(matfile, mode='r') as f:
+                ftimes = f['ftimes'][:]
+                if len(ftimes.shape) != 1:
+                    ftimes = ftimes.flatten()
 
-    np.savez(os.path.join(exp_dir, 'frametimes', name + '_frametimes'),
-             f_on=ftimes)
-    print(f'Converted and saved frametimes for {name}')
+        ftimes = (ftimes/1000)+monitor_delay
+
+        np.savez(os.path.join(exp_dir, 'frametimes', name + '_frametimes'),
+                 f_on=ftimes)
+        print(f'Converted and saved frametimes for {name}')
