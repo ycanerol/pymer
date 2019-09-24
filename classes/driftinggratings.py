7f0cfb417c4be1b57abde3f1b58250813b2e45ff
Yunus Can Erol <ycanerol@gmail.com>
2019-09-24 12:40:40 +0200

    Add tuning curve and DSI calculation for drifting gratings

--- classes/driftinggratings.py
+++ classes/driftinggratings.py
@@ -22,6 +22,10 @@ class DriftingGratings(Stimulus):
 
         self.readpars()
         self._setdefaults()
+        self.reshape_frametimings()
+        self.calculate_tuning_curves()
+        self.vectorize_tuning_curves()
+        self.calculate_dsi()
 
     def _setdefaults(self):
         pars = Parameters()
@@ -35,9 +39,56 @@ class DriftingGratings(Stimulus):
         pars.gratingwidth = param_file.get('gratingwidth', None)
         pars.duration = param_file.get('duration', 5*pars.period)
 
-        pars.angles = np.linspace(0, 2*np.pi, num=pars.nangles, endpoint=False)
+        pars.nperiod = int(pars.duration / pars.period)
+        pars.pulse_per_cycle = pars.nperiod * pars.nangles
+        ncycles = int(np.floor(self.frametimings.shape[0] / pars.pulse_per_cycle))
+
+        if ncycles != pars.ncycles:
+            raise ValueError('Unexpected number of cycles.')
+
+        self.angles = np.linspace(0, 2*np.pi, num=pars.nangles, endpoint=False)
+        self.angles_wrapped = self.wrap_first_element(self.angles)
 
         self.pars = pars
+
+    def wrap_first_element(self, array):
+        """
+        Add the first element of an array to the end for closing polar plots.
+        """
+        return np.hstack((array, array[0]))
+
+    def reshape_frametimings(self):
+        ft = self.frametimings.copy()
+        ft = ft[:(self.pars.pulse_per_cycle*self.pars.ncycles)]
+        ft_shape = (self.pars.nperiod, self.pars.nangles, self.pars.ncycles)
+        ft = ft.reshape(ft_shape)
+        self.frametimings_rs = ft
+        self.ft_shape = ft_shape
+
+    def calculate_tuning_curves(self):
+        binned_spikes = np.zeros((self.nclusters, *self.ft_shape))
+        allspikes = self.allspikes()
+        for i in range(self.nclusters):
+            binned_spikes[i, ...] = allspikes[i, :].reshape(*self.ft_shape)
+        self.tuning_curves_unwrapped = binned_spikes.mean(axis=(1, 3))
+        self.tuning_curves = np.hstack((self.tuning_curves_unwrapped,
+                                        self.tuning_curves_unwrapped[:, 0][:, None]))
+
+    def plot_tuning_curve(self, i):
+        plt.polar(self.angles_wrapped, self.tuning_curves[i, :])
+        plt.plot([0, self.vec_t[i]], [0, self.vec_r[i]], 'k')
+
+    def vectorize_tuning_curves(self):
+        y_all = np.sin(self.angles)[None, ...] * self.tuning_curves_unwrapped
+        x_all = np.cos(self.angles)[None, ...] * self.tuning_curves_unwrapped
+        y = y_all.sum(axis=1)
+        x = x_all.sum(axis=1)
+        self.vec_r = np.hypot(x, y)
+        self.vec_t = np.arctan2(y, x)  # HINT: order of y and x is important
+
+    def calculate_dsi(self):
+        dsi = self.vec_r/self.tuning_curves_unwrapped.sum(axis=1)
+        self.dsi = dsi
 
 if __name__ == '__main__':
     exp, stimnr = 'Kuehn', 5
