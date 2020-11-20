146cec9402d3bd6a68a1e83c84c0bf702338e324
ycanerol <ycanerol@gmail.com>
2020-11-20 14:01:43 +0100

    Exclude cells that are not spiking and plot in manual CCA

--- cca/cca_macke.py
+++ cca/cca_macke.py
@@ -22,6 +22,12 @@ spikes = st.allspikes()
 # Set the mean to zero for spikes
 #spikes -= spikes.mean(axis=1)[:, None]
 
+# Exclude rows that have no spikes throughout
+nonzerospikerows = ~np.isclose(spikes.sum(axis=1), 0)
+spikes = spikes[nonzerospikerows, :]
+
+ncells = spikes.shape[0]
+
 bgsteps = st.bgsteps
 
 
@@ -44,11 +50,42 @@ for i in range(stspcov.shape[0]):
                 (spikes[:, j] - np.mean(spikes[:, j]))
                                 )
 #%%
+ncomponents = min(stimulus.shape[1], spikes.shape[1])
 
 stimcov_exp = fractional_matrix_power(stimcov, -0.5)
 spkcov_exp = fractional_matrix_power(spkcov, -0.5)
 whitened_cov =  stimcov_exp @ stspcov @ spkcov_exp
 
-u, s, vh = np.linalg.svd(whitened_cov, full_matrices=True)
+u, s, v = np.linalg.svd(whitened_cov, full_matrices=True)
 
+# rows of v correspond to components, to interpret it in the same way,
+# we transpose it
+v = v.T
 print('done')
+
+# SVD returns a much larger matrix than needed for the larger matrix, we take
+# only the needed part.
+respcomps = v[:, :ncomponents].reshape((ncells, filter_length[1], ncomponents))
+# Reshape so that the order is: ncomponents, ncells, time
+respcomps = np.moveaxis(respcomps, [2, 0, 1], [0, 1, 2])
+
+stimcomps = u.reshape((2, filter_length[0], ncomponents))
+stimcomps = np.moveaxis(stimcomps, [2, 0, 1], [0, 1, 2])
+#%%
+
+fig, axes = plt.subplots(3, 2)
+ax0 = axes[0, 0]
+ax1 = axes[0, 1]
+ax2 = axes[1, 0]
+ax3 = axes[1, 1]
+ax4 = axes[2, 0]
+ax0.imshow(u)
+ax1.imshow(v)
+ax2.plot(s, 'ok')
+ax2.set_title('Singular values')
+
+# ax3.imshow(v[:, 0].reshape((ncells, filter_length[1])))
+ax3.imshow(respcomps[0, ...])
+ax4.plot(stimcomps[0, ...].T)
+plt.show()
+
